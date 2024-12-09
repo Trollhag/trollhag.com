a697101e85064e6f2409ef2473cd2107288f8a39
Oscar Trollhag <oscar@trollhag.com>
2024-12-09 23:03:37 +0100

    removed sanity for gh pages

--- schema/index.ts
+++
@@ -1,6 +0,0 @@
-import blocks from './blocks'
-import content from './blocks/content'
-import card from './blocks/card'
-import page from './page'
-
-export const schemaTypes = [page, content, card, blocks]
